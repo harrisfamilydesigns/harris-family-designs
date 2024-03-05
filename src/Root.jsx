1287a8a91053cba36b3961592da2d4380648c684
Max Harris <mharris7190@gmail.com>
2024-03-04 23:02:16 -0500

    hashrouter

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminLoginPage from './components/AdminLoginPage';
 import LandingPage from './components/LandingPage';
 import AdminDashboardPage from './components/AdminDashboardPage';
