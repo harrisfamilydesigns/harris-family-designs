8386c522f6d88876d9c3916ffbd93aaacd4f31e0
Max Harris <mharris7190@gmail.com>
2024-06-19 21:22:43 -0400

    map api to onrender.com

--- src/api/config/constants.js
+++ src/api/config/constants.js
@@ -1,4 +1,4 @@
 export const HFD_API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://harrisfamilydesigns.com/api/v1'
+  ? 'harris-family-designs-backend.onrender.com/api/v1'
   : 'http://localhost:3000/api/v1';
 
