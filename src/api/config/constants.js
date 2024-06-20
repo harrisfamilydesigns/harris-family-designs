1b260a06c708a355eb95f544f604d96b645ab6c2
Max Harris <mharris7190@gmail.com>
2024-06-19 20:49:50 -0400

    update 4444 -> 3000

--- src/api/config/constants.js
+++ src/api/config/constants.js
@@ -1,4 +1,4 @@
 export const HFD_API_URL = process.env.NODE_ENV === 'production'
   ? 'https://harrisfamilydesigns.com/api/v1'
-  : 'http://localhost:4444/api/v1';
+  : 'http://localhost:3000/api/v1';
 
