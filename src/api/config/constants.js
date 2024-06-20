31f841b8ac15f124368907c49c9d2c2a7602061e
Max Harris <mharris7190@gmail.com>
2024-06-20 15:26:49 -0400

    fix production api endpoint

--- src/api/config/constants.js
+++ src/api/config/constants.js
@@ -1,4 +1,4 @@
 export const HFD_API_URL = process.env.NODE_ENV === 'production'
-  ? 'api.harrisfamilydesigns.com/api/v1'
+  ? 'https://api.harrisfamilydesigns.com/api/v1'
   : 'http://localhost:3000/api/v1';
 
