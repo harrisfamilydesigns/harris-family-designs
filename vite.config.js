9d6b365a752ad821e615d9d44d70745b12d67a5c
Max Harris <mharris7190@gmail.com>
2024-10-08 23:06:33 -0400

    add cert and https --host command to allow location access on local network

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: '/src',
-      components: '/src/components',
-      assets: '/src/assets',
-      styles: '/src/styles',
-      hooks: '/src/hooks',
-      constants: '/src/constants',
-      api: '/src/api',
-      utils: '/src/utils',
-      helpers: '/src/helpers',
-      controllers: '/src/api/controllers',
+export default defineConfig(({ command, mode }) => {
+  const useHttps = process.env.VITE_HTTPS === 'true'
+
+  return {
+    server: useHttps ? {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
+      },
+    } : false,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: '/src',
+        components: '/src/components',
+        assets: '/src/assets',
+        styles: '/src/styles',
+        hooks: '/src/hooks',
+        constants: '/src/constants',
+        api: '/src/api',
+        utils: '/src/utils',
+        helpers: '/src/helpers',
+        controllers: '/src/api/controllers',
+      }
     }
   }
 })
